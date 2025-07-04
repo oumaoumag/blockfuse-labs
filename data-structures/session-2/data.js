743d10da68a9c41168200e55b14af40e066e7ae9
oumouma oumouma <garveyshah1595@egmail.com>
2025-07-04 13:35:41 +0300

    Implement Stack class with push method and basic structure

---
+++ data-structures/session-2/data.js
@@ -0,0 +1,17 @@
+class Stack {
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
+
+    PushManager(item) {
+        this.items[this.length] = item
+        this.length += 1
+    }
+
+    // pop(item) {
+    //     this.items[]
+    // }
+}
+
+pu
